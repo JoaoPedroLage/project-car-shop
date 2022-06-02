1d64467d1fe7cf14e6ad06d0bbbfa33bf6745390
JoaoPedroLage <JoaoPedro.Lage@outlook.com>
2022-06-02 14:56:05 -0300

    fix: add 'mongodb' to mongoDatabaseURI

--- src/connection.ts
+++ src/connection.ts
@@ -1,9 +1,12 @@
 import mongoose from 'mongoose';
 import 'dotenv/config';
 
+// const MONGO_DB_URL = 'mongodb://localhost:27017/CarShop';
+const MONGO_DB_URL = 'mongodb://mongodb:27017/CarShop';
+
 const connectToDatabase = (
   mongoDatabaseURI = process.env.MONGO_URI
-    || 'mongodb://localhost:27017/CarShop',
+    || MONGO_DB_URL,
 ) => mongoose.connect(mongoDatabaseURI);
 
 export default connectToDatabase;
