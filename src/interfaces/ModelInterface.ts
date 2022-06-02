02182c9d791898340bb09adff6c1a4f57f244498
JoaoPedroLage <JoaoPedro.Lage@outlook.com>
2022-06-02 11:58:09 -0300

    feat: create a Interface to use for Models

--- src/interfaces/ModelInterface.ts
+++ src/interfaces/ModelInterface.ts
@@ -1,7 +1,7 @@
 export interface Model<T> {
   create(obj:T): Promise<T>;
   read(): Promise<T[]>;
-  readOne(name: string): Promise<T | null>;
-  update(name: string, obj: T): Promise<T | null>;
-  delete(name: string): Promise<T | null>;
+  readOne(id: string): Promise<T | null>;
+  update(id: string, obj: T): Promise<T | null>;
+  delete(id: string): Promise<T | null>;
 }
